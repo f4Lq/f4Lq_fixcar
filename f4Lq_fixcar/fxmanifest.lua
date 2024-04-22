f083af7d01120e534ad41f2b56dae0a7e2163cf4
f4Lq <167790412+f4Lq@users.noreply.github.com>
2024-04-22 17:32:48 +0200

    Update fxmanifest.lua

--- f4Lq_fixcar/fxmanifest.lua
+++ f4Lq_fixcar/fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'cerulean'
 lua54 'yes'
 game 'gta5'
+
+description 'This is a script that allows you to repair your vehicle using the "/fixcar" command. The script needs the "dopeNotify2" script for it to work properly.'
+
 name 'falq_fixcar'
 author 'faLq'
-description 'This is a script that allows you to repair your vehicle using the "/fixcar" command. The script needs the "dopeNotify2" script for it to work properly.'
 version '1.1.0'
-
 
 client_script {
     'fixcar.lua',
