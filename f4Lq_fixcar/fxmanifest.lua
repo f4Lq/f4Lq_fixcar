d6e65b71db53e947ffa3cee0a004aa0612facff2
f4Lq <167790412+f4Lq@users.noreply.github.com>
2024-05-26 14:17:05 +0200

    Update fxmanifest.lua

--- f4Lq_fixcar/fxmanifest.lua
+++ f4Lq_fixcar/fxmanifest.lua
@@ -5,12 +5,15 @@ game 'gta5'
 name 'f4lq_fixcar'
 author 'f4Lq'
 description 'This is a script that allows you to repair your vehicle using the "/fixcar" command. The script needs the "f4LqNotify" script for it to work properly.'
-version '1.3.0'
+version '1.4.0'
 
 
 client_script {
-    'fixcar.lua',
+    'fixcar_cl.lua',
 }
 
+server_script {
+    'fixcar_sv.lua',
+}
 
 dependency 'f4LqNotify'
